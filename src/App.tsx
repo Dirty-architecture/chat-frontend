5cc701ff554b7850b435539683836bcfd0b7bae8
zelld <zeLLa444@gmail.com>
2026-01-21 19:58:32 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type {ReactNode} from 'react';
+
+const App = (): ReactNode => {
+    return (
+        <div>
+            asd
+        </div>
+    );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), tsconfigPaths(),],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                includePaths: [path.resolve(__dirname, "src")],
+                additionalData: `@use "@app/styles/variables" as *;`
+            },
+        },
+    },
+})
