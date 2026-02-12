706d45a868fbaece08207d3264e7be614dc41b73
jakiichu <zeLLda444@gmail.com>
2026-02-13 00:53:59 +0700

    add scss imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from 'path';
+import {pathToFileURL} from "node:url";
 
 // https://vite.dev/config/
 
@@ -11,6 +12,17 @@ export default defineConfig({
         preprocessorOptions: {
             scss: {
                 loadPaths: [path.resolve(__dirname, "src")],
+                importers: [
+                    {
+                        findFileUrl(url) {
+                            if (url.startsWith("@/")) {
+                                const absPath = path.resolve(__dirname, "src", url.slice(2));
+                                return pathToFileURL(absPath);
+                            }
+                            return null;
+                        },
+                    },
+                ],
                 additionalData: `
           @use "app/common/styles/variables" as *;
         `
