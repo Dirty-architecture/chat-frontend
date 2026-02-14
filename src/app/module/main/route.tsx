05732c26f1410e185d47782e4faf83f7f1159fc0
jakiichu <zeLLda444@gmail.com>
2026-02-14 13:08:29 +0700

    migrate not-open-chat component to main module and update imports accordingly

--- src/app/module/main/route.tsx
+++ src/app/module/main/route.tsx
@@ -2,7 +2,7 @@ import {type ReactNode} from 'react';
 import {useMediaQuery} from "@/app/utils/useMediaQuery.ts";
 import {BREAKPOINTS} from "@/app/common/const/breakpoints.ts";
 import ChatSidebar from "@/app/module/general/chat-sidebar";
-import {NotOpenChat} from "@/app/module/basic-structure/case/chat-content";
+import {NotOpenChat} from "./not-open-chat";
 import s from './index.module.scss'
 
 const MainRoute = (): ReactNode => {
