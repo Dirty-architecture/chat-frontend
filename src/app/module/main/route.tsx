7795e8ef81b957f6c46f6acb3b041215d1bf2713
jakiichu <zeLLda444@gmail.com>
2026-02-16 16:39:19 +0700

    rename `mediaQuery` to `useMediaQuery` for clarity and update all usage references

--- src/app/module/main/route.tsx
+++ src/app/module/main/route.tsx
@@ -1,5 +1,5 @@
 import {type ReactNode} from 'react';
-import {mediaQuery} from "@/app/utils/media-query.ts";
+import {useMediaQuery} from "@/app/utils/media-query.ts";
 import {BREAKPOINTS} from "@/app/common/const/breakpoints.ts";
 import ChatSidebar from "@/app/module/general/chat-sidebar";
 import {NotOpenChat} from "./not-open-chat";
@@ -7,7 +7,7 @@ import s from './index.module.scss'
 
 const MainRoute = (): ReactNode => {
 
-    const isLaptop = mediaQuery(`(max-width: ${BREAKPOINTS.laptop}px)`);
+    const isLaptop = useMediaQuery(`(max-width: ${BREAKPOINTS.laptop}px)`);
 
     return (
         <>
